fa2ab21c5a8ef12d5dfcf775e6e402a209d7c3af
Vito <victornberg@gmail.com>
2023-07-25 20:17:33 -0300

    Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Hello User, Welcome to the home page</h1>
+</template>
+<script>
+export default {
+    name: 'Home-component',
+       mounted(){
+        let user = localStorage.getItem('user-info');
+        if(!user){
+            this.$router.push({name:'Signup'})
+        }
+    }
+}
+</script>
