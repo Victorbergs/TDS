9912a7940716e622a1542a69e84c7a8b84b916be
Vito <victornberg@gmail.com>
2023-07-26 09:52:48 -0300

    Commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -15,8 +15,8 @@
                 <td>{{ item.name }}</td>
                 <td>{{ item.contact }}</td>
                 <td>{{ item.address }}</td>
-                <td><router-link :to="'/update/'+item.id">Update</router-link></td>
-
+                <td><router-link :to="'/update/' + item.id">Update</router-link></td>
+                <button v-on:click="deleteRestaurant(item.id)">Delete</button>
             </tr>
         </table>
     </div>
@@ -35,15 +35,27 @@ export default {
     components: {
         Header
     },
-    async mounted() {
-        let user = localStorage.getItem('user-info');
-        this.name = JSON.parse(user).name;
-        if (!user) {
-            this.$router.push({ name: 'Signup' })
+    methods: {
+        async deleteRestaurant(id) {
+            let result = await axios.delete('http://localhost:3000/restaurant/'+id)
+            console.warn(result)
+            if (result.status == 200) {
+                this.loadData();
+            }
+        },
+        async loadData() {
+            let user = localStorage.getItem('user-info');
+            this.name = JSON.parse(user).name;
+            if (!user) {
+                this.$router.push({ name: 'Signup' })
+            }
+            let result = await axios.get('http://localhost:3000/restaurant');
+            this.restaurant = result.data;
         }
-        let result = await axios.get('http://localhost:3000/restaurant');
-        console.warn(result)
-        this.restaurant = result.data;
+    },
+
+    async mounted() {
+        this.loadData();
     }
 }
 </script>
--- src/components/Update.vue
+++ src/components/Update.vue
@@ -5,11 +5,12 @@
         <input type="text" name="name" placeholder="Enter Name" v-model="restaurant.name" />
         <input type="text" name="address" placeholder="Enter address" v-model="restaurant.address" />
         <input type="text" name="contact" placeholder="Enter contact" v-model="restaurant.contact" />
-        <button type="button" v-on:click="addRestaurant">Update restaurant</button>
+        <button type="button" v-on:click="updateRestaurant">Update restaurant</button>
     </form>
 </template>
 <script>
 import Header from './Header.vue'
+import axios from 'axios';
 export default {
     name: 'Update-component',
     components: {
@@ -24,11 +25,27 @@ export default {
             }
         }
     },
-    mounted() {
+    methods: {
+        async updateRestaurant() {
+            const result = await axios.put("http://localhost:3000/restaurant/"+this.$route.params.id, {
+                name: this.restaurant.name,
+                address: this.restaurant.address,
+                contact: this.restaurant.contact,
+            });
+            if (result.status == 200) {
+                this.$router.push({ name: 'Home' });
+            }
+        }
+    },
+    async mounted() {
         let user = localStorage.getItem('user-info');
         if (!user) {
             this.$router.push({ name: 'Signup' })
         }
+        const result = await axios.get('http://localhost:3000/restaurant/' + this.$route.params.id)
+        //console.warn(this.$route.params.id)
+        console.warn(result.data)
+        this.restaurant = result.data
     }
 }
 </script>
